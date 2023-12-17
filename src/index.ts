1a2a099db9cbd54b5fb607107dc8ee32b4cd4b23
Abhranil Ray <abhranil794@gmail.com>
2023-12-17 10:19:42 +0530

    fix environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import http from "http";
-import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
 import router from "./router";
+
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
@@ -26,8 +27,7 @@ server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/`);
 });
 
-const MONGO_URL =
-  "mongodb+srv://abhranil:VXVa42v7vWlel2Ci@cluster0.t27sjxn.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
